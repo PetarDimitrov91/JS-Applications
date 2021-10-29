fc64be6016c741768f43e6348f1275b029b967e6
Petar Dimitrov <79804094+PetarDimitrov91@users.noreply.github.com>
2021-10-29 22:28:05 +0200

    dasdasda

--- 2. Exercises/4. Exercise Asynchronous Programming/01.Bus-Stop/app.js
+++ 2. Exercises/4. Exercise Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,15 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const inputRef = document.getElementById('stopId');
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${inputRef.value}`;
+    const outputRef = document.getElementById('stopName');
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+    } catch (ex) {
+        outputRef.textContent = ex.message;
+    }
+
+
 }
