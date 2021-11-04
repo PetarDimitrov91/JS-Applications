fd2de085d538500e606a9598cee10586dab4948f
Petar Dimitrov <79804094+PetarDimitrov91@users.noreply.github.com>
2021-11-04 21:52:39 +0100

    dasdas

--- 2. Exercises/6. Remote Data and Authentication Exercise/01.Messenger/app.js
+++ 2. Exercises/6. Remote Data and Authentication Exercise/01.Messenger/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+    const messagesArea = document.getElementById('messages');
+    messagesArea.value = '';
+
+    window.addEventListener('DOMContentLoaded', displayMessages);
+
+    const nameInput = document.querySelector('[name=author]');
+    const messageInput = document.querySelector('[name=content]');
+    const sendBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    sendBtn.addEventListener('click', sendMessage);
+    refreshBtn.addEventListener('click', displayMessages);
+
+    async function sendMessage() {
+        const message = {
+            author: nameInput.value,
+            content: messageInput.value
+        };
+
+        nameInput.value = '';
+        messageInput.value = '';
+
+        const options = {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(message)
+        };
+
+        await Promise.all([fetch(url, options), displayMessages()]);
+    }
+
+    async function displayMessages() {
+        const messages = Object.values(await loadMessages()).map(e => `${e.author}: ${e.content}`);
+        messagesArea.value = messages.join('\n');
+    }
+
+    async function loadMessages() {
+        const response = await fetch(url);
+        return await response.json();
+    }
 }
 
 attachEvents();
+
+
+
